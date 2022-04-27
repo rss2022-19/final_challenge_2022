7c7488e0d1e9189281c1dc6d9d2b52c50ffbdcbe
Mary Lau <marylau@mit.edu>
2022-04-27 00:55:08 -0400

    Add CMakelist and package.xml to run lane_detector.

---
+++ final_race/scripts/homography_transformer.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from ackermann_msgs.msg import AckermannDriveStamped
+from visualization_msgs.msg import Marker
+from final_race.msg import GoalPixel, GoalPoint
+
+#The following collection of pixel locations and corresponding relative
+#ground plane locations are used to compute our homography matrix
+
+# PTS_IMAGE_PLANE units are in pixels
+# see README.md for coordinate frame description
+
+######################################################
+## DUMMY POINTS -- ENTER YOUR MEASUREMENTS HERE
+PTS_IMAGE_PLANE = [[172.0, 210.0],
+                   [263.0, 230.0],
+                   [408.0, 274.0],
+                   [459.0, 223.0]] # dummy points
+
+# PTS_IMAGE_PLANE = [[173.0, 208.0],
+#                    [246.0, 186.0],
+#                    [371.0, 246.0],
+#                    [430.0, 202.0]] # dummy points
+
+######################################################
+
+# PTS_GROUND_PLANE units are in inches
+# car looks along positive x axis with positive y axis to left
+
+######################################################
+## DUMMY POINTS -- ENTER YOUR MEASUREMENTS HERE
+PTS_GROUND_PLANE = [[1/.0254, 0.61/.0254],
+                    [.79/.0254, .26/.0254],
+                    [.54/.0254, -.04/.0254],
+                    [1/.0254, -.26/.0254]] # dummy points
+
+# PTS_GROUND_PLANE = [[48.0, 25.0],
+#                     [69.5, 21.0],
+#                     [28.5, 0],
+#                     [50.5, -11.0]] # dummy points                    
+######################################################
+
+METERS_PER_INCH = 0.0254
+
+
+class HomographyTransformer:
+    def __init__(self):
+        self.cone_px_sub = rospy.Subscriber("/relative_cone_px", GoalPixel, self.cone_detection_callback)
+        self.cone_pub = rospy.Publisher("/relative_cone", GoalPoint, queue_size=10)
+
+        self.marker_pub = rospy.Publisher("/cone_marker",
+            Marker, queue_size=1)
+
+        if not len(PTS_GROUND_PLANE) == len(PTS_IMAGE_PLANE):
+            rospy.logerr("ERROR: PTS_GROUND_PLANE and PTS_IMAGE_PLANE should be of same length")
+
+        #Initialize data into a homography matrix
+
+        np_pts_ground = np.array(PTS_GROUND_PLANE)
+        np_pts_ground = np_pts_ground * METERS_PER_INCH
+        np_pts_ground = np.float32(np_pts_ground[:, np.newaxis, :])
+
+        np_pts_image = np.array(PTS_IMAGE_PLANE)
+        np_pts_image = np_pts_image * 1.0
+        np_pts_image = np.float32(np_pts_image[:, np.newaxis, :])
+
+        self.h, err = cv2.findHomography(np_pts_image, np_pts_ground)
+
+    def cone_detection_callback(self, msg):
+        #Extract information from message
+        u = msg.u
+        v = msg.v
+
+        #Call to main function
+        x, y = self.transformUvToXy(u, v)
+
+        #Publish relative xy position of object in real world
+        relative_xy_msg = GoalPoint()
+        relative_xy_msg.x_pos = x
+        relative_xy_msg.y_pos = y
+
+        self.cone_pub.publish(relative_xy_msg)
+
+
+    def transformUvToXy(self, u, v):
+        """
+        u and v are pixel coordinates.
+        The top left pixel is the origin, u axis increases to right, and v axis
+        increases down.
+
+        Returns a normal non-np 1x2 matrix of xy displacement vector from the
+        camera to the point on the ground plane.
+        Camera points along positive x axis and y axis increases to the left of
+        the camera.
+
+        Units are in meters.
+        """
+        homogeneous_point = np.array([[u], [v], [1]])
+        xy = np.dot(self.h, homogeneous_point)
+        scaling_factor = 1.0 / xy[2, 0]
+        homogeneous_xy = xy * scaling_factor
+        x = homogeneous_xy[0, 0]
+        y = homogeneous_xy[1, 0]
+        return x, y
+
+    def draw_marker(self, cone_x, cone_y, message_frame):
+        """
+        Publish a marker to represent the cone in rviz.
+        (Call this function if you want)
+        """
+        marker = Marker()
+        marker.header.frame_id = message_frame
+        marker.type = marker.CYLINDER
+        marker.action = marker.ADD
+        marker.scale.x = .2
+        marker.scale.y = .2
+        marker.scale.z = .2
+        marker.color.a = 1.0
+        marker.color.r = 1.0
+        marker.color.g = .5
+        marker.pose.orientation.w = 1.0
+        marker.pose.position.x = cone_x
+        marker.pose.position.y = cone_y
+        self.marker_pub.publish(marker)
+
+
+if __name__ == "__main__":
+    rospy.init_node('homography_transformer')
+    homography_transformer = HomographyTransformer()
+    print("x,y = ", homography_transformer.transformUvToXy(183, 82))
+    rospy.spin()
---
+++ final_race/scripts/image_processing.py
@@ -0,0 +1,78 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def find_goal_point(rgb_img, display=False):
+    """
+    returns: (u, v) pixel point for goal to follow 
+    """
+
+    COLOR_POINT = (0, 255, 0)
+    COLOR_LINES = (0, 0, 255)
+    
+    ## THRESHOLDING
+    # create a zero array
+    stencil = np.zeros_like(rgb_img[:,:,0])
+    # specify coordinates of the polygon
+    #polygon = np.array([[-150,376], [280,160], [400,160], [1100,376]])
+    polygon = np.array([[0,376], [0,150], [672,150], [672,376]])
+    # fill polygon with ones
+    cv2.fillConvexPoly(stencil, polygon, 1)
+    cropped_rgb_img = cv2.bitwise_and(rgb_img, rgb_img, mask=stencil)
+    # apply image thresholding
+    thresh, ret = cv2.threshold(cv2.cvtColor(cropped_rgb_img, cv2.COLOR_RGB2GRAY), 165, 200, cv2.THRESH_BINARY)
+
+    ## HOUGH TRANSFORM TO GET LINES
+    lines = cv2.HoughLinesP(ret, 1, np.pi/180, 60, maxLineGap=200)
+    lines_filtered = []
+    # create a copy of the original frame
+    # dmy = image[:,:,0].copy()
+    rbg_img_copy = rgb_img.copy()
+    # draw Hough lines
+    for line in lines:
+      x1, y1, x2, y2 = line[0]
+      m = (y2-y1)/(x2-x1)
+      if (abs(m) > 0.25):
+          lines_filtered.append(line)
+          cv2.line(rbg_img_copy, (x1, y1), (x2, y2), COLOR_LINES, 3)
+
+#     # PREVIOUS CODE
+#     y_lookahead = 255
+#     sumx = 0
+#     for line in lines_filtered:
+#         x1, y1, x2, y2 = line[0]
+#         m = (y2-y1)/(x2-x1)
+#         b = y2-(m*x2)
+#         x_lookahead = (y_lookahead-b)/m
+#         sumx += x_lookahead
+    
+#     avgx = sumx/(len(lines_filtered))
+#     test_image = rbg_img_copy.copy()
+#     cv2.circle(test_image, (int(avgx), y_lookahead), 5, (0, 0, 255), 3)
+#     #########
+    
+
+    ## GET LANE POINT WITH LOOKAHEAD RANGE
+    y_lookahead_min, y_lookahead_max = 255, 256
+    y_lookahead_range = range(y_lookahead_min, y_lookahead_max)
+    sumx = 0;
+    for y_lookahead in y_lookahead_range:
+        for line in lines_filtered:
+            x1, y1, x2, y2 = line[0]
+            m = (y2-y1)/(x2-x1)
+            b = y2-(m*x2)
+            x_lookahead = (y_lookahead-b)/m
+            sumx += x_lookahead
+    
+    avgx = int(sumx/(len(lines_filtered) * (y_lookahead_max-y_lookahead_min)))
+
+    avgy = int((y_lookahead_min+y_lookahead_max) / 2.0)
+    
+    cv2.circle(rbg_img_copy, (avgx, avgy), 5, COLOR_POINT, 3)
+    
+    if display==True:
+        plt.imshow(rbg_img_copy)
+        plt.show()
+    
+    return (avgx, avgy)
